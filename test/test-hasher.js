372431aeede408022ffe7a85c4e2b4e4cf51c492
C J Silverio <ceejceej@gmail.com>
2013-12-31 13:10:57 -0800

    Enable testing on Travis. Node 0.11 tests error on Travis due to installation problems that are, I believe, Travis's fault.

--- test/test-hasher.js
+++ test/test-hasher.js
@@ -5,31 +5,25 @@ var XXHash = require('../lib/xxhash');
 
 var seed = 0xcafed00d;
 
-describe('XXHash', function()
-{
-	it('requires a seed passed to its constructor', function()
-	{
+describe('XXHash', function() {
+	it('requires a seed passed to its constructor', function() {
 		function shouldThrow() { return new XXHash(); }
 		shouldThrow.must.throw(/Expected unsigned integer seed argument/);
 	});
 
-	it('can be constructed', function()
-	{
+	it('can be constructed', function() {
 		var hasher = new XXHash(seed);
 		hasher.must.be.an(XXHash);
 	});
 
-	it('has an update() method', function()
-	{
+	it('has an update() method', function() {
 		var hasher = new XXHash(seed);
 		hasher.must.have.property('update');
 		hasher.update.must.be.a.function();
 	});
 
-	it('update() requires a buffer argument', function()
-	{
-		function shouldThrow()
-		{
+	it('update() requires a buffer argument', function() {
+		function shouldThrow() {
 			var hasher = new XXHash(seed);
 			hasher.update('foo');
 		}
@@ -37,48 +31,42 @@ describe('XXHash', function()
 		shouldThrow.must.throw(/data argument must be a Buffer/);
 	});
 
-	it('update() can be called repeatedly', function()
-	{
+	it('update() can be called repeatedly', function() {
 		var hasher = new XXHash(seed);
 		hasher.update(new Buffer('foo'));
 		hasher.update(new Buffer('bar'));
 	});
 
-	it('has a digest() method', function()
-	{
+	it('has a digest() method', function() {
 		var hasher = new XXHash(seed);
 		hasher.must.have.property('digest');
 		hasher.digest.must.be.a.function();
 	});
 
-	it('returns an integer from digest()', function()
-	{
+	it('returns an integer from digest()', function() {
 		var hasher = new XXHash(seed);
 		hasher.update(new Buffer('foo'));
 		var result = hasher.digest();
 		result.must.be.a.number();
 	});
 
-	function hashHelper(input)
-	{
+	function hashHelper(input) {
 		var hasher = new XXHash(seed);
 		hasher.update(new Buffer(input));
 		return hasher.digest();
 	}
 
-	it('hashes to the expected results', function()
-	{
+	it('hashes to the expected results', function() {
 		var inputs = [ 'bar', 'coatis have long tails'];
 		var results = [ 2370523005, 1792348589];
 
-		for (var i = 0; i < inputs.length; i++)
+		for (var i = 0; i < inputs.length; i++) {
 			hashHelper(inputs[i]).must.equal(results[i]);
+    }
 	});
 
-	it('update() cannot be called after digest() has been called', function()
-	{
-		function shouldThrow()
-		{
+	it('update() cannot be called after digest() has been called', function() {
+		function shouldThrow() {
 			var hasher = new XXHash(seed);
 			hasher.update(new Buffer('foo'));
 			hasher.digest();
@@ -87,4 +75,4 @@ describe('XXHash', function()
 
 		shouldThrow.must.throw(/cannot call update after digest/);
 	});
-})
+});
