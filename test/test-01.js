16c28a04b0ff2507203d391db1f76c5afabe9a6a
C J Silverio <ceejceej@gmail.com>
2013-12-31 09:22:34 -0800

    An almost adequate set of tests, including one test that fails because the behavior varies from the original, one test that is stubbed out because both the original binding & this one have behavior I consider unsatisfactory, and one moderately inadequate set of hash results tests.

---
+++ test/test-01.js
@@ -0,0 +1,92 @@
+/*global describe:true, it:true, before:true, after:true */
+
+var demand = require('must');
+var XXHash = require('../lib/xxhash');
+
+var seed = 0xcafed00d;
+
+describe('XXHash', function()
+{
+	it('requires a seed passed to its constructor', function()
+	{
+		function shouldThrow() { return new XXHash(); }
+		shouldThrow.must.throw(/Expected unsigned integer seed argument/);
+	});
+
+	it('can be constructed', function()
+	{
+		var hasher = new XXHash(seed);
+		hasher.must.be.an(XXHash);
+	});
+
+	it('has an update() method', function()
+	{
+		var hasher = new XXHash(seed);
+		hasher.must.have.property('update');
+		hasher.update.must.be.a.function();
+	});
+
+	it('update() requires a buffer argument', function()
+	{
+		function shouldThrow()
+		{
+			var hasher = new XXHash(seed);
+			// hasher.update('foo');
+		}
+
+		shouldThrow.must.throw(/data argument must be a Buffer/);
+	});
+
+	it('update() can be called repeatedly', function()
+	{
+		var hasher = new XXHash(seed);
+		hasher.update(new Buffer('foo'));
+		hasher.update(new Buffer('bar'));
+	});
+
+	it('has a digest() method', function()
+	{
+		var hasher = new XXHash(seed);
+		hasher.must.have.property('digest');
+		hasher.digest.must.be.a.function();
+	});
+
+	it('returns an integer from digest()', function()
+	{
+		var hasher = new XXHash(seed);
+		hasher.update(new Buffer('foo'));
+		var result = hasher.digest();
+		result.must.be.a.number();
+	});
+
+	function hashHelper(input)
+	{
+		var hasher = new XXHash(seed);
+		hasher.update(new Buffer(input));
+		return hasher.digest();
+	}
+
+	it('hashes to the expected results', function()
+	{
+		var inputs = [ 'bar', 'coatis have long tails'];
+		var results = [ 2370523005, 1792348589];
+
+		for (var i = 0; i < inputs.length; i++)
+			hashHelper(inputs[i]).must.equal(results[i]);
+	});
+
+	it('update() cannot be called after digest() has been called' /*, function()
+	{
+		// Original bindings just SIGSEGV here. Would be better to detect & throw instead.
+
+		function shouldThrow()
+		{
+			var hasher = new XXHash(seed);
+			hasher.update(new Buffer('foo'));
+			hasher.digest();
+			// hasher.update(new Buffer('bar'));
+		}
+
+		shouldThrow.must.throw();
+	} */);
+})
